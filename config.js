962ff255414372f2fbeb90cecb6a7ec12da10bbd
mrsachiya <144035888+mrsachiya@users.noreply.github.com>
2023-09-05 07:24:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94761860465', 'me', true],
+  ['94761860465', 'MR.SACHIYA', true], 
   [''] 
 ] //Number of owners
 
